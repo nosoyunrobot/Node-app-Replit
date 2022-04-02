ccaa63056adbed062188351b183611f4e7e0bcf2
Non4me <57117406+nosoyunrobot@users.noreply.github.com>
2022-04-02 05:35:12 +0000

    creando rutas

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000
 
-app.listen( port , () => console.log('Server running in port'))
+
+app.get("/" , (req, res) => {
+  res.send('Bivenido crack');
+})
+
+app.listen( port , () => console.log(`Server running in port ${port}`))
